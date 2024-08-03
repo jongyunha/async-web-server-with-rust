3762c93bd8a2f79351bb714f92649114950beddb
jongyunha <jongyun@seoulrobotics.org>
2024-08-03 21:09:05 +0900

    feat: init simple webserver

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::str::FromStr;
+use std::io::{Error, ErrorKind};
+use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::get()
+        .map(|| "Hello, World!".to_string());
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
